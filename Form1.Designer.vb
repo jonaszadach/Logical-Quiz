d56b18a2ba88f21c336e0597ad8b121b6b2ad91f
Jonas Zadach <jonas.zadach@web.de>
2016-08-18 14:58:28 +0200

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSchwuchtel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSchwuchtel
+        '
+        Me.btnSchwuchtel.Location = New System.Drawing.Point(200, 100)
+        Me.btnSchwuchtel.Margin = New System.Windows.Forms.Padding(10, 9, 10, 9)
+        Me.btnSchwuchtel.Name = "btnSchwuchtel"
+        Me.btnSchwuchtel.Size = New System.Drawing.Size(525, 361)
+        Me.btnSchwuchtel.TabIndex = 1
+        Me.btnSchwuchtel.Text = "Traust du dich, den Schwuchtel-Eid zu machen? Schaffst du es, den Türsteher mit r" &
+    "ichtigen Antworten auszutricksen? Antworte niemals falsch!"
+        Me.btnSchwuchtel.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1354, 733)
+        Me.Controls.Add(Me.btnSchwuchtel)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(10, 9, 10, 9)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSchwuchtel As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub btnSchwuchtel_Click(sender As Object, e As EventArgs) Handles btnSchwuchtel.Click
+        Dim dr As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "SCHWUCHTEL-EID", MessageBoxButtons.YesNo)
+        If dr = DialogResult.Yes Then
+            MessageBox.Show("jHerr K.Lopapier, Haha sie sind wirklich eine Schwuchtel!a")
+            MessageBox.Show("Eine sehr große sogar sie Klopapier!!")
+        Else
+            MessageBox.Show("jDu bist trotzdem eine Karsten Lopapier!!!a")
+            MessageBox.Show("Eine sehr große sogar Karsten!!")
+        End If
+
+        Dim df As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Ja oder Nein?", MessageBoxButtons.YesNo)
+        If df = DialogResult.Yes Then
+            MessageBox.Show("Richtige Antwort! Hier sind überall nur Schwuchteln verdammt noch mal ... Nada ")
+        Else
+            MessageBox.Show("Falsche Antwort, Hast du das JA übersehen? Es bezog sich auf die nächste Frage. Versuch es nochmal! Na endlich mal einer der hier keine Schwuchtel ist, danke K.Lopapier !!! NEIN")
+        End If
+        Dim da As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Ja oder Nein?", MessageBoxButtons.YesNo)
+        If da = DialogResult.Yes Then
+            MessageBox.Show("Falsche Antwort, hast du das NEIN eben übersehen? Man man man bist du schlecht in diesem Spiel! Ich gebe bald meinen Geist auf hier als Schwuchtel-Wärter, hier sind nur Lügner und Heuchler NEIN")
+        Else
+            MessageBox.Show("Richtige Antwort! Aber Du lügst K.Lopapier! Sag die Wahrheit oder stirb. Also ? NEIN ")
+        End If
+        Dim db As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Ja oder Nein?", MessageBoxButtons.YesNo)
+        If db = DialogResult.Yes Then
+            MessageBox.Show("Bist du der Ja-Sager ? Hör auf damit! das war erneut die falsche Antwort JA JA JA JA JAAA")
+        Else
+            MessageBox.Show("Richtig geantwortet. Moment... Was? Nein? Du bist hier nur am lügen, Egal was du sagst, du nicht sprechen die Wahrheit K.Lopapier JAJAJAJAJAJA")
+        End If
+        Dim dc As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Welche Antwort?", MessageBoxButtons.YesNo)
+        If dc = DialogResult.Yes Then
+            MessageBox.Show("Richtige Antwort Jawoll!!! Mr. Herr Professor Dr. med. K.Lopapier.... das hier ist der Eingang zu einem Waschhaus, sie sind falsch ACHTUNG HINT ---DRÜCKE NEIN--- HINT")
+        Else
+            MessageBox.Show("Sie sollten vielleicht noch mal die Spielregeln durch lesen Mr. Herr Professor Dr. med. K.Lopapier.... das hier ist der Eingang zu einem Toilettenhaus im Freien, sie sind falsch ACHTUNG HINT ---DRÜCKE NEIN--- HINT")
+        End If
+        Dim de As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Ja oder Nein?", MessageBoxButtons.YesNo)
+        If de = DialogResult.Yes Then
+            MessageBox.Show("Du wagst es erneut JA zu drücken ohne meine Erlaubnis??? Sorry, es ist mein Auftrag öfters zu fragen, um sicher zu gehen, Also ja, sie sind eine Schwuchtel entgültig ALLES FALSCH FUCK du hast 1 Freeticket für das Spiel gewonnen durch falsche Antworten, You're Welcome Amigo")
+            MessageBox.Show("Hier sind überall nur Schwuchteln verdammt noch mal ...")
+        Else
+            MessageBox.Show("Du hast tatsächlich auf NEIN gedrück... Sorry, es ist mein Auftrag öfters zu fragen, um sicher zu gehen, Also nein, sie sind entgültig keine Schwuchtel ALLES FALSCH FUCK du bekommst 1 Freeticket für das Spiel weil du falsche Antworten gegeben hast... oder nicht? You're Welcome Amigo")
+            MessageBox.Show("Na endlich mal einer der hier keine Schwuchtel ist, danke K.Lopapier !!!")
+        End If
+        Dim dd As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "Ja oder Nein?", MessageBoxButtons.YesNo)
+        If dd = DialogResult.Yes Then
+            MessageBox.Show("Das Ende ist fast erreicht. Ein hin und her! So Mir reichts. Ich kündige! Suchen sie sich einen anderen Türsteher des Örtlichen Klo-Verbandes")
+            MessageBox.Show("Das ist der totale Unsinn hier!!! Wer hat sich dieses kack Spiel ausgedacht")
+            MessageBox.Show("Jonas Zadach aus Laatzen Germany")
+            MessageBox.Show("Den knöpfe ich mir vor")
+            MessageBox.Show("Man wird hier nur verarscht! Was für eine Verarschung schon wieder")
+            MessageBox.Show("Seit 7 Jahren mache ich das hier mit Türstehen für Toilettenbenutzer im Öfftentlichen und alles was ich hier sehe Sind Transsexuelle Schwuchteln! Igitt")
+            MessageBox.Show("Vielen Dank, dass sie das neue Action-Spiel Türsteher vom FESTIVAL DEVELOPMENT gespielt haben, bis bald. Copyright D2016 Jonas Zadach Festival Development")
+        Else
+            MessageBox.Show("Ist ein mal 16 wirklich 108? Ich würde sagen NEIN! Immer diese Ungenauen Antworten! So mir reichts !!! Ich kündige !!! Suchen sie sich einen neuen Türsteher!")
+            MessageBox.Show("Das ist der totale Unsinn hier!!! Wer hat sich dieses kack Spiel ausgedacht")
+            MessageBox.Show("Jonas Zadach aus Laatzen Germany nahe Hannover....")
+            MessageBox.Show("Den knöpfe ich mir vor")
+            MessageBox.Show("Ich mache das hier jetzt schon seit sieben Jahren! Jeden Tag werden neue Schwule zum Himmeltor kommen")
+            MessageBox.Show("Vielen Dank, dass sie das neue Action-Spiel Türsteher vom FESTIVAL DEVELOPMENT gespielt haben, bis bald. Copyright D2016 Jonas Zadach Festival Development")
+        End If
+        Dim dz As DialogResult = MessageBox.Show(DirectCast(sender, Button).Text, "", MessageBoxButtons.YesNo)
+        If dz = DialogResult.Yes Then
+            MessageBox.Show("FALSCH")
+            MessageBox.Show("VERLOREN, NOCHMAL VERSUCHEN... und ne Schwuchtel biste trotzdem du homo, hmmmmmmmmm bis zum nächsten Versuch")
+        Else
+            MessageBox.Show("richtig.... aber VERSUCHS NOCH MAL, DAS WAR KEIN FEHLERFREIER WEG")
+            MessageBox.Show("Dicke Schwuchtel biste trotzdem du Homo! Bis zum nächsten mal!")
+        End If
+    End Sub
+
+    Private Sub Form1_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        Me.BackColor = Color.LightGreen
+    End Sub
+End Class
